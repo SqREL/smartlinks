bd59721f707ca61c0611641246a84a36d1a372a0
Vasilij Melnychuk <isqrel@gmail.com>
2013-02-16 15:19:57 +0200

    Tests green :green_heart: Need refactoring

--- lib/smartlinks/base.rb
+++ lib/smartlinks/base.rb
@@ -6,6 +6,8 @@ module Smartlinks
   PAT_EMAIL = %r{
     [a-z0-9!#$\%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$\%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?
   }x
+  PAT_ACCOUNT = %r{(^|\s)@\w+}
+  PAT_HASH = %r{#\w+}
 
   def self.linkify(text, params={})
     text.gsub! PAT_EMAIL do |email|
@@ -16,11 +18,32 @@ module Smartlinks
       "<a href=\"#{url}\">#{url}</a>"
     end
 
-    text
-  end
-  
-  def twitter_account(text, params={})
-    text.gsub!(/@\w+/) { |twitter| "https://twitter.com/#{twitter}" }
+    text.gsub!(PAT_ACCOUNT) do |account|
+      whitespace = ""
+      whitespace = " " if account.strip!
+      account.slice!(0)
+
+      if params[:account]
+        url = params[:account].sub "%s", account
+        "#{whitespace}<a href=\"#{url}\">@#{account}</a>"
+      else
+        "#{whitespace}<a href=\"https://twitter.com/#{account}\">@#{account}</a>"
+      end
+    end
+
+    text.gsub!(PAT_HASH) do |hash|
+      whitespace = ""
+      whitespace = " " if hash.strip!
+      hash.slice!(0)
+
+      if params[:hash]
+        url = params[:hash].sub "%s", hash
+        "#{whitespace}<a href=\"#{url}\">##{hash}</a>"
+      else
+        "#{whitespace}<a href=\"https://twitter.com/search?q=%23#{hash}&src=hash\">##{hash}</a>"
+      end
+    end
+
     text
   end
 end
--- spec/lib/smartlinks/smartlinks_spec.rb
+++ spec/lib/smartlinks/smartlinks_spec.rb
@@ -4,48 +4,60 @@ describe Smartlinks do
 
   it "modifies http url" do
     text, result = data "http"
-    Smartlinks::linkify(text).should eql(result)
+    Smartlinks.linkify(text).should eql(result)
   end
   it "modifies https url" do
     text, result = data "https"
-    Smartlinks::linkify(text).should eql(result)
+    Smartlinks.linkify(text).should eql(result)
   end
   it "modifies ftp url" do
     text, result = data "ftp"
-    Smartlinks::linkify(text).should eql(result)
+    Smartlinks.linkify(text).should eql(result)
   end
   it "modifies http url" do
     text, result = data "http"
-    Smartlinks::linkify(text).should eql(result)
+    Smartlinks.linkify(text).should eql(result)
   end
 
   it "do not modifyes if allready link" do
     text = "Allready <a href=\"http://google.com\">linkifyed</a> url"
-    Smartlinks::linkify(text).should eql(text)
+    Smartlinks.linkify(text).should eql(text)
   end
 
   it "modifyes plaintext mail account to link" do
     text = "Reply to admin@test.com and check user@gmail.com account"
     result = "Reply to <a href=\"mailto:admin@test.com\">admin@test.com</a> and check <a href=\"mailto:user@gmail.com\">user@gmail.com</a> account"
-    Smartlinks::linkify(text).should eql(result)
+    Smartlinks.linkify(text).should eql(result)
   end
 
   it "creates twitter links" do
     text = "@sqrel, check out @dht twitter"
-    result = "<a href=\"https://twitter.com/sqrel\">@sqrel</a>, check out <a href=\"https://twitter.com/dht\">@dht</a> twitter</a>"
-    Smartlinks::linkify(text).should eql(result)
+    result = "<a href=\"https://twitter.com/sqrel\">@sqrel</a>, check out <a href=\"https://twitter.com/dht\">@dht</a> twitter"
+    Smartlinks.linkify(text).should eql(result)
   end
 
   it "creates twitter-like links with custom domain" do
     text = "@sqrel, check out @dht account"
-    result = "<a href=\"http://test.com/sqrel\">@sqrel</a>, check out <a href=\"http://test.com/dht\">@dht</a> account</a>"
-    Smartlinks::linkify(text, url: "http://test.com").should eql(result)
+    result = "<a href=\"http://test.com/sqrel\">@sqrel</a>, check out <a href=\"http://test.com/dht\">@dht</a> account"
+    Smartlinks.linkify(text, account: "http://test.com/%s").should eql(result)
+  end
+
+  it "creates twitter hash links" do
+    text = "#dude, check out #rails framework"
+    result = "<a href=\"https://twitter.com/search?q=%23dude&src=hash\">#dude</a>, check out <a href=\"https://twitter.com/search?q=%23rails&src=hash\">#rails</a> framework"
+    Smartlinks::linkify(text).should eql(result)
+  end
+
+  it "creates twitter-like links with custom domain" do
+    text = "#dude, check out #rails framework"
+    result = "<a href=\"http://example.com/dude?src=hash\">#dude</a>, check out <a href=\"http://example.com/rails?src=hash\">#rails</a> framework"
+    Smartlinks::linkify(text, hash: "http://example.com/%s?src=hash").should eql(result)
   end
 
   private
     def data(protocol)
-      text = "Word #{protocol}://lorem.com/id/1 seperator #{protocol}://test.com/foo/bar Foo"
-      result = "Word <a href=\"#{protocol}://lorem.com/id/1\">#{protocol}://lorem.com/id/1</a> seperator <a href=\"#{protocol}://test.com/foo/bar\">#{protocol}://test.com/foo/bar</a> Foo"
+      text = "#{protocol}://example.com/3 Word #{protocol}://lorem.com/id/1 seperator #{protocol}://test.com/foo/bar Foo"
+      result = "<a href=\"#{protocol}://example.com/3\">#{protocol}://example.com/3</a> Word <a href=\"#{protocol}://lorem.com/id/1\">#{protocol}://lorem.com/id/1</a> seperator <a href=\"#{protocol}://test.com/foo/bar\">#{protocol}://test.com/foo/bar</a> Foo"
       [text, result]
     end
 
